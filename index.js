f89bcfcd54f25efc9cea8e5e83422da15170b4c6
maslah <Maslah-mohamed1789>
2024-10-17 15:58:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", (e)=>{
+    e.preventDefault();
+    
+    const mealsLists = document.querySelector(".meals-lists");
+    function getMeals(){
+        fetch("http://localhost:3000/recipes")
+        .then((r) => r.json())
+        .then((data) => data.forEach(meals => {
+            const meal = document.createElement("div");
+            meal.classList.add("meal-container");
+            meal.innerHTML = `
+                    <img src="${meals.image}" alt="" class="meal">
+                    <span class="mealname">${meals.name}</span>
+                    <button class="show" id="view">View Recipe</button>
+                `
+            meal.querySelector(".show").addEventListener("click",()=>{
+                fetch(`http://localhost:3000/recipes/${meals.id}`)
+                .then((r)=> r.json())
+                .then((data) => {
+                    const instraction = document.createElement("div");
+                    instraction.classList.add("intractions");
+                    instraction.innerHTML = `
+                    <div class="mealnamecd">${data.name}</div>
+                    <div class="instruction">
+                        <h1>instructions:</h1>
+                        <p class="instruction">
+                            ${data.instructions}
+                        </p>
+                    </div>`;
+                    document.body.appendChild(instraction);
+                    document.body.addEventListener("click", ()=>{
+                        instraction.remove();
+                    })
+                })
+                
+
+            })
+
+            mealsLists.appendChild(meal);
+        }))
+    }
+    getMeals()
+})
